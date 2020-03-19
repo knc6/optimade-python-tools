b030a3e394fc5a192de420b68afebbdebffa5fd0
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-03-19 17:21:50 +0100

    Go through parameters using while loop

--- run.sh
+++ run.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 
 LOG_LEVEL=info
-if [ "$1" == "debug" ]; then
-    export OPTIMADE_DEBUG=1
-    LOG_LEVEL=debug
-fi
-
-if [ "$1" == "index" ]; then
-    MAIN="main_index"
-    PORT=5001
-    if [ "$2" == "debug" ]; then
+start_regular_server=false
+start_index_server=false
+while [ $# -gt 0 ]; do
+    if [ "$1" == "debug" ]; then
         export OPTIMADE_DEBUG=1
         LOG_LEVEL=debug
+    elif [ "$1" == "regular" ]; then
+        start_regular_server=true
+    elif [ "$1" == "index" ]; then
+        start_index_server=true
     fi
-else
-    if [ "${MAIN}" == "main_index" ]; then
-        PORT=5001
-    else
-        MAIN="main"
-        PORT=5000
-    fi
+    shift
+done
+
+if [ "$start_regular_server" == "false" ] && [ "$start_index_server" == "false" ]; then
+    start_regular_server=true
+    start_index_server=true
 fi
 
-uvicorn optimade.server.$MAIN:app --reload --port $PORT --log-level $LOG_LEVEL
+if [ "$start_regular_server" == "true" ]; then
+    MAIN=main
+    PORT=5000
+    echo ""
+    uvicorn optimade.server.$MAIN:app --reload --port $PORT --log-level $LOG_LEVEL &
+fi
+if [ "$start_index_server" == "true" ]; then
+    MAIN=main_index
+    PORT=5001
+    echo ""
+    uvicorn optimade.server.$MAIN:app --reload --port $PORT --log-level $LOG_LEVEL &
+fi
